d377b403102a9c431c2ec0546fdd801f9950039d
Marco Orfei <marcoasrorfei@gmail.com>
2020-07-27 16:31:27 +0200

    Delete secret

--- app/core/settings.py
+++ app/core/settings.py
@@ -2,11 +2,19 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    """
+    App settings
+    """
+
     PROJECT_NAME: str = "FastAPI Example"
     ACCESS_TOKEN_EXPIRES_MINUTES: int = 30
-    SECRET_KEY: str = "83e241b98872a68640365c9d3f05da463f2ee38b7aea83ceb204fb628c70fd09"
+    SECRET_KEY: str = "<run: openssl rand -hex 32>"
 
     class Config:
+        """
+        Provide configurations to Pydantic
+        """
+
         case_sensitive = True
 
 
