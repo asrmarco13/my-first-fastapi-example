5ab27cd049094d6dadf95ae1b5d095d11819dba3
Marco Orfei <marcoasrorfei@gmail.com>
2020-09-11 17:49:10 +0200

    Update database settings

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from core.settings import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<postgres-server>:<port>/<db>"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- app/core/settings.py
+++ app/core/settings.py
@@ -1,4 +1,5 @@
-from pydantic import BaseSettings
+from typing import Optional, Any, Dict
+from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
@@ -9,6 +10,25 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI Example"
     ACCESS_TOKEN_EXPIRES_MINUTES: int = 30
     SECRET_KEY: str = "<run: openssl rand -hex 32>"
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: str
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("DB_USER"),
+            password=values.get("DB_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('DB_NAME') or ''}",
+        )
 
     class Config:
         """
@@ -16,6 +36,8 @@ class Settings(BaseSettings):
         """
 
         case_sensitive = True
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()
